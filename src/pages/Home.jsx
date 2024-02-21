147a4b1c60d757f5662055eae80ca64955842b56
Evanglemer Sebial <glemerzkie@gmail.com>
2024-02-21 23:20:45 +0800

    Updating

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,23 @@
 import React from 'react';
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '../assets/vite.svg'
 import '../App.css';
 const Home = () => {
     const [count, setCount] = useState(0)
   return (
-    <div>  <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+        <div className='w-full border-2 border-b-white border-t-transparent border-l-transparent border-r-transparent h-18'>
+            <h1 className="text-5xl text-black font-bold text-left p-4">Welcome</h1>
+        </div>  
+        <div  className="flex flex-col items-center justify-around mt-4">
+        <form className='mt-2 border-white border-2 rounded-md border-solid p-2 w-96'action="">
+            <label>Welome</label><br/><br />
+            <label>Username</label><br />
+            <input type="text" className='w-2/3 h-10 rounded-md p-2' name='Username' placeholder='Username'/><br/>
+            <label>Password</label><br />
+            <input type="password" className='w-2/3 h-10 rounded-md p-2' name='Password' placeholder='Password'/><br/>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      </div>
+    </div>
   )
 }
 
